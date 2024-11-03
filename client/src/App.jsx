7b55bbad2f9bedae6ae54af0623e80327fb9d32e
asw <annasilverwolff@gmail.com>
2024-11-02 22:24:49 -0400

    removed placeholder html
    placeholder navbar wasnt displaying anything, simplest solution is to just remove it and start from scratch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,10 @@ function App() {
   return (
     <>
 
+    <>
     <Navbar/>
+    </>
+  
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
